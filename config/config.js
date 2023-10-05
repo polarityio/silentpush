f80576ac0432bd04454a3da34c316fcc4aaff1ea
Nick Smith <nicktsmith7@gmail.com>
2023-10-05 11:42:38 -0700

    feat: initial commit

--- config/config.js
+++ config/config.js
@@ -1,19 +1,43 @@
 module.exports = {
-    name: 'Silent Push',
-    acronym: 'SP',
-    description: 'Enrich alerts with Whois, Risk Scores, and much more context from Silent Push Threat Intelligence',
-    defaultColor: 'dark-blue-gray',
-    entityTypes: ['IPv4', 'IPv6', 'domain', 'url'],
-    styles: ['./styles/main.css'],
-    options: [
-        {
-          key: 'apiKey',
-          name: 'API Key',
-          description: "If you don't have an api key yet, sign up on https://explore.silentpush.com/register",
-          default: '',
-          type: 'password',
-          userCanEdit: true,
-          adminOnly: true
-        },
-    ]
+  name: "Silent Push",
+  acronym: "SP",
+  request: {
+    // Provide the path to your certFile. Leave an empty string to ignore this option.
+    cert: "",
+    // Provide the path to your private key. Leave an empty string to ignore this option.
+    key: "",
+    // Provide the key passphrase if required.  Leave an empty string to ignore this option.
+    passphrase: "",
+    // Provide the Certificate Authority. Leave an empty string to ignore this option.
+    ca: "",
+    // An HTTP proxy to be used. Supports proxy Auth with Basic Auth, identical to support for
+    // the url parameter (by embedding the auth info in the uri)
+    proxy: "",
+  },
+  logging: { level: "trace" },
+  description:
+    "Enrich alerts with Whois, Risk Scores, and much more context from Silent Push Threat Intelligence",
+  defaultColor: "dark-blue-gray",
+  entityTypes: ["IPv4", "IPv6", "domain", "url"],
+  styles: ["./styles/main.css"],
+  block: {
+    component: {
+      file: "./components/component.js",
+    },
+    template: {
+      file: "./templates/template.hbs",
+    },
+  },
+  options: [
+    {
+      key: "apiKey",
+      name: "API Key",
+      description:
+        "If you don't have an api key yet, sign up on https://explore.silentpush.com/register",
+      default: "",
+      type: "password",
+      userCanEdit: true,
+      adminOnly: true,
+    },
+  ],
 };
--- integration.js
+++ integration.js
@@ -1,143 +1,173 @@
-const request = require('request');
-const async = require('async');
-require('dotenv').config();
+const request = require("request");
+const async = require("async");
+require("dotenv").config();
 
 let Logger;
-let apiKey = '';
+let apiKey = "";
 let SILENTPUSH_API_URL;
 
-function startup(logger){
-    Logger = logger;
-    SILENTPUSH_API_URL = process.env.SILENTPUSH_API_URL || "https://app.silentpush.com/api/"
+function startup(logger) {
+  Logger = logger;
+  SILENTPUSH_API_URL =
+    process.env.SILENTPUSH_API_URL || "https://app.silentpush.com/api/";
 }
 
 doLookup = (entities, options, cb) => {
-    Logger.info(entities);
-    apiKey = options.apiKey;
-    let lookupResults = [];
-    async.each(entities, function(entity, next){
-        if(entity.isIPv4) {
-            enrichIPv4(entity, function (err, result) {
+  Logger.info(entities);
+  apiKey = options.apiKey;
+  let lookupResults = [];
+  async.each(
+    entities,
+    function (entity, next) {
+      if (entity.isIPv4) {
+        enrichIPv4(entity, function (err, result) {
+          if (!err) {
+            lookupResults.push(result); // add to our results if there was no error
+          }
+          next(err); // processing complete
+        });
+      } else if (entity.isDomain) {
+        enrichDomain(entity, function (err, result) {
+          if (!err) {
+            lookupResults.push(result);
+          }
+          next(err);
+        });
+      } else if (entity.isURL) {
+        parseIoC(entity.value, function (parsedIoC) {
+          Logger.info(`parsedIoC: ${parsedIoC}`);
+          entity.value = parsedIoC;
+          enrichDomain(entity, (err, result) => {
+            if (!err) {
+              lookupResults.push(result); // add to our results if there was no error
+            } else {
+              enrichIPv4(entity, (err, result) => {
                 if (!err) {
-                    lookupResults.push(result); // add to our results if there was no error
-                }
-                next(err);  // processing complete
-            });
-        }else if(entity.isDomain) {
-            enrichDomain(entity, function (err, result) {
-                if (!err) {
-                    lookupResults.push(result);
+                  lookupResults.push(result);
                 }
                 next(err);
-            });
-        }else if(entity.isURL){
-            parseIoC(entity.value, function (parsedIoC) {
-                Logger.info(`parsedIoC: ${parsedIoC}`);
-                entity.value = parsedIoC;
-                enrichDomain(entity, (err, result) => {
-                    if (!err) {
-                        lookupResults.push(result); // add to our results if there was no error
-                    } else {
-                        enrichIPv4(entity, (err, result) => {
-                            if (!err) {
-                                lookupResults.push(result);
-                            }
-                            next(err);
-                        });
-                    }
-                    next(err);
-                });
-            });
-        }else{
-            next(null);
-        }
-    }, function(err){
-        cb(err, lookupResults);
-    });
-}
+              });
+            }
+            next(err);
+          });
+        });
+      } else {
+        next(null);
+      }
+    },
+    function (err) {
+      Logger.trace({ lookupResults: lookupResults }, "lookupResults");
+      cb(err, lookupResults);
+    }
+  );
+};
 
 enrichIPv4 = (entity, done) => {
-    Logger.info(entity);
-    request(getEnrichmentURI(entity), function(err, response, body){
-        Logger.info(`enrichIPv4 error: ${err}`);
-        Logger.info(`enrichIPv4 response: ${JSON.stringify(response)}`);
-        Logger.info(`enrichIPv4 body: ${JSON.stringify(body)}`);
-        if(err || response.statusCode !== 200){
-            // return either the error object, or the body as an error
-            done(err || body);
-            return;
-        }
-        // there was no error in making the GET request so process the body here
-        done(null, {
-            entity: entity,
-            data:{
-                summary: [entity.value],
-                details: body.response
-            }
-        });
+  Logger.info(entity);
+  request(getEnrichmentURI(entity), function (err, response, body) {
+    Logger.info(`enrichIPv4 error: ${err}`);
+    Logger.info(`enrichIPv4 response: ${JSON.stringify(response)}`);
+    Logger.info(`enrichIPv4 body: ${JSON.stringify(body)}`);
+    if (err || response.statusCode !== 200) {
+      // return either the error object, or the body as an error
+      done(err || body);
+      return;
+    }
+    // there was no error in making the GET request so process the body here
+    done(null, {
+      entity: entity,
+      data: {
+        summary: summary(body.response),
+        details: body.response,
+      },
     });
-}
+  });
+};
 
 enrichDomain = (entity, done) => {
-    Logger.info(entity);
-    request(getEnrichmentURI(entity, "domain"), function(err, response, body){
-        Logger.info(`enrichDomain error: ${err}`);
-        Logger.info(`enrichDomain response: ${JSON.stringify(response)}`);
-        Logger.info(`enrichDomain body: ${JSON.stringify(body)}`);
-        if(err || response.statusCode !== 200){
-            done(err || body);
-            return;
-        }
-        done(null, {
-            entity: entity,
-            data:{
-                summary: [entity.value],
-                details: body.response
-            }
-        });
+  Logger.info(entity);
+  request(getEnrichmentURI(entity, "domain"), function (err, response, body) {
+    Logger.info(`enrichDomain error: ${err}`);
+    Logger.info(`enrichDomain response: ${JSON.stringify(response)}`);
+    Logger.info(`enrichDomain body: ${JSON.stringify(body)}`);
+    if (err || response.statusCode !== 200) {
+      done(err || body);
+      return;
+    }
+    done(null, {
+      entity: entity,
+      data: {
+        summary: summary(body.response),
+        details: {
+          domainData: body.response,
+        },
+      },
     });
-}
+  });
+};
 
 getEnrichmentURI = (entity, type = "ipv4") => {
-    const enrichment_url = SILENTPUSH_API_URL +
-        `v1/merge-api/explore/enrich/${type}/${entity.value}` +
-        '?explain=1&scan_data=1&with_metadata=1&query_type=Enrichment&' +
-        'query_origin=ENRICHMENT&is_voluntary=1';
-    return {
-        url: enrichment_url,
-        json: true,
-        // verify: false,
-        headers: {
-            "X-Api-Key": apiKey,
-            "User-Agent": "PolarityIO"
-        }
-    };
-}
+  const enrichment_url =
+    SILENTPUSH_API_URL +
+    `v1/merge-api/explore/enrich/${type}/${entity.value}` +
+    "?explain=1&scan_data=1&with_metadata=1&query_type=Enrichment&" +
+    "query_origin=ENRICHMENT&is_voluntary=1";
+  return {
+    url: enrichment_url,
+    json: true,
+    // verify: false,
+    headers: {
+      "X-Api-Key": apiKey,
+      "User-Agent": "PolarityIO",
+    },
+  };
+};
 
 parseIoC = (ioc, done) => {
-    const uri = {
-        url: SILENTPUSH_API_URL + 'v2/utils/parse-ioc/',
-        body: {ioc: ioc},
-        json: true,
-        // verify: false,
-        headers: {
-            "X-Api-Key": apiKey,
-            "User-Agent": "PolarityIO"
-        }
-    };
-    request.post(uri, function(err, response, body){
-        Logger.info(`parseIoC error: ${err}`);
-        Logger.info(`parseIoC response: ${JSON.stringify(response)}`);
-        Logger.info(`parseIoC body: ${JSON.stringify(body)}`);
-        done(body.result);
-    });
+  const uri = {
+    url: SILENTPUSH_API_URL + "v2/utils/parse-ioc/",
+    body: { ioc: ioc },
+    json: true,
+    // verify: false,
+    headers: {
+      "X-Api-Key": apiKey,
+      "User-Agent": "PolarityIO",
+    },
+  };
+  request.post(uri, function (err, response, body) {
+    Logger.info(`parseIoC error: ${err}`);
+    Logger.info(`parseIoC response: ${JSON.stringify(response)}`);
+    Logger.info(`parseIoC body: ${JSON.stringify(body)}`);
+    done(body.result);
+  });
+};
+
+function summary(response) {
+  const tags = [];
+
+  if (response && response.ip2asn && response.ip2asn.length > 0) {
+    for (const ip of response.ip2asn) {
+      tags.push(`ASN: ${ip.asn}`);
+      tags.push(`SP Risk Score: ${ip.sp_risk_score}`);
+      tags.push(`Subnet: ${ip.subnet}`);
+    }
+  }
+
+  if (response && response.domain_string_frequency_probability) {
+    tags.push(`SP Risk Score: ${response.sp_risk_score}`);
+    if (response && response.domain_string_frequency_probability.whois_age) {
+      tags.push(`WhoIs Age: ${response.domaininfo.whois_age}`);
+    }
+
+    if (response && response.domain_string_frequency_probability.registrar) {
+      tags.push(`Registrar: ${response.domaininfo.registrar}`);
+    }
+  }
+
+  return tags;
 }
 
-// function onDetails(lookupObject, options, callback)
-// function onMessage(payload, options, cb)
-// function validateOptions(userOptions, cb)
-
 module.exports = {
-    startup: startup,
-    doLookup: doLookup
+  startup: startup,
+  doLookup: doLookup,
 };
