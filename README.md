414c38e108c866c5c883258bdf116068e93d06bd
Nick Smith <nicktsmith7@gmail.com>
2023-11-27 20:36:50 -0800

    feat: initial-commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# SilentPush - Polarity Integration
+
+The **Silent Push** - Polarity Integration allow users to pull contextual information from Silent Push API, such as:
+
+- Name Server reputation
+- Name Server changes
+- Who Is
+- Risk Score
+- ASN
+- Certificates
+- ...and much more!
+
+## Configuration
+
+If you don't have a Silent Push account yet, go to https://explore.silentpush.com/register
+
+All you need is just set the API Key in the integration options
+
+## Silent Push URL
+
+The URL for your Silent Push instance.
+
+## Silent Push API Key
+
+If you don't have an api key yet, sign up on https://explore.silentpush.com/register
+
+## Installation Instructions
+
+Installation instructions for integrations are provided on the [PolarityIO GitHub Page](https://polarityio.github.io/).
+
+## Polarity
+
+Polarity is a memory-augmentation platform that improves and accelerates analyst decision making. For more information about the Polarity platform please see:
+
+https://polarity.io/
---
+++ integration.js
@@ -0,0 +1,193 @@
+const request = require('postman-request');
+const async = require('async');
+
+require('dotenv').config();
+
+let Logger;
+let apiKey = '';
+
+function startup(logger) {
+  Logger = logger;
+}
+
+doLookup = (entities, options, cb) => {
+  Logger.info(entities);
+  apiKey = options.apiKey;
+  let lookupResults = [];
+  async.each(
+    entities,
+    function (entity, next) {
+      if (entity.isIPv4) {
+        enrichIPv4(entity, options, function (err, result) {
+          if (!err) {
+            lookupResults.push(result); // add to our results if there was no error
+          }
+          next(err); // processing complete
+        });
+      } else if (entity.isDomain) {
+        enrichDomain(entity, options, function (err, result) {
+          if (!err) {
+            lookupResults.push(result);
+          }
+          next(err);
+        });
+      } else if (entity.isURL) {
+        parseIoC(
+          entity.value,
+          function (parsedIoC) {
+            Logger.info(`parsedIoC: ${parsedIoC}`);
+            entity.value = parsedIoC;
+            enrichDomain(entity, options, (err, result) => {
+              if (!err) {
+                lookupResults.push(result); // add to our results if there was no error
+              } else {
+                enrichIPv4(entity, options, (err, result) => {
+                  if (!err) {
+                    lookupResults.push(result);
+                  }
+                  next(err);
+                });
+              }
+              next(err);
+            });
+          },
+          options
+        );
+      } else {
+        next(null);
+      }
+    },
+    function (err) {
+      Logger.trace({ lookupResults: lookupResults }, 'lookupResults');
+      cb(err, lookupResults);
+    }
+  );
+};
+
+enrichIPv4 = (entity, options, done) => {
+  Logger.info(entity);
+  request(getEnrichmentURI(entity, 'ipv4', options), function (err, response, body) {
+    Logger.info(`enrichIPv4 error: ${err}`);
+    Logger.info(`enrichIPv4 response: ${JSON.stringify(response)}`);
+    Logger.info(`enrichIPv4 body: ${JSON.stringify(body)}`);
+    if (err || response.statusCode !== 200) {
+      // return either the error object, or the body as an error
+      done(err || body);
+      return;
+    }
+    // there was no error in making the GET request so process the body here
+    done(null, {
+      entity: entity,
+      data: {
+        summary: summary(body.response),
+        details: body.response
+      }
+    });
+  });
+};
+
+enrichDomain = (entity, options, done) => {
+  Logger.info(entity);
+  request(getEnrichmentURI(entity, 'domain', options), function (err, response, body) {
+    Logger.info(`enrichDomain error: ${err}`);
+    Logger.info(`enrichDomain response: ${JSON.stringify(response)}`);
+    Logger.info(`enrichDomain body: ${JSON.stringify(body)}`);
+    if (err || response.statusCode !== 200) {
+      done(err || body);
+      return;
+    }
+    done(null, {
+      entity: entity,
+      data: {
+        summary: summary(body.response),
+        details: {
+          domainData: body.response
+        }
+      }
+    });
+  });
+};
+
+getEnrichmentURI = (entity, type = 'ipv4', options) => {
+  const enrichment_url =
+    'https://app.silentpush.com/api' +
+    `/v1/merge-api/explore/enrich/${type}/${entity.value}` +
+    '?explain=1&scan_data=1&with_metadata=1&query_type=Enrichment&' +
+    'query_origin=ENRICHMENT&is_voluntary=1';
+  return {
+    url: enrichment_url,
+    json: true,
+    // verify: false,
+    headers: {
+      'X-Api-Key': apiKey,
+      'User-Agent': 'PolarityIO'
+    }
+  };
+};
+
+parseIoC = (ioc, done, options) => {
+  const uri = {
+    url: options.url + '/v2/utils/parse-ioc/',
+    body: { ioc: ioc },
+    json: true,
+    // verify: false,
+    headers: {
+      'X-Api-Key': apiKey,
+      'User-Agent': 'PolarityIO'
+    }
+  };
+  request.post(uri, function (err, response, body) {
+    Logger.info(`parseIoC error: ${err}`);
+    Logger.info(`parseIoC response: ${JSON.stringify(response)}`);
+    Logger.info(`parseIoC body: ${JSON.stringify(body)}`);
+    done(body.result);
+  });
+};
+
+function summary(response) {
+  const tags = [];
+
+  if (response && response.ip2asn && response.ip2asn.length > 0) {
+    for (const ip of response.ip2asn) {
+      tags.push(`ASN: ${ip.asn}`);
+      tags.push(`SP Risk Score: ${ip.sp_risk_score}`);
+      tags.push(`Subnet: ${ip.subnet}`);
+    }
+  }
+
+  if (response && response.domain_string_frequency_probability) {
+    tags.push(`SP Risk Score: ${response.sp_risk_score}`);
+    if (response && response.domaininfo.whois_age) {
+      tags.push(`WhoIs Age: ${response.domaininfo.whois_age}`);
+    }
+
+    if (response && response.domaininfo.registrar) {
+      tags.push(`Registrar: ${response.domaininfo.registrar}`);
+    }
+  }
+
+  return tags;
+}
+
+function validateOptions(userOptions, cb) {
+  const errors = [];
+
+  if (
+    typeof userOptions.apiKey.value !== 'string' ||
+    (typeof userOptions.apiKey.value === 'string' &&
+      userOptions.apiKey.value.length === 0)
+  ) {
+    errors.push({
+      key: 'apiKey',
+      message: 'You must provide a valid API Key'
+    });
+  }
+
+  cb(null, errors);
+}
+
+module.exports = {
+  startup: startup,
+  validateOptions: validateOptions,
+  doLookup: doLookup
+};
